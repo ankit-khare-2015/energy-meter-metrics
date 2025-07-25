a803f630c3b10c0ea2c143fb9a4572a03960b228
Ankit Khare <ankit.khare.2015@gmail.com>
2025-07-25 21:51:56 +0200

    removed all the auto-generated dbt files

--- dbt_project/target/compiled/energy_analytics/models/staging/stg_households.sql
+++
@@ -1,7 +0,0 @@
--- models/staging/stg_households.sql
-SELECT
-  household_id,
-  city,
-  meter_id,
-  installation_date
-FROM "analytics"."public"."households"
--- dbt_project/target/compiled/energy_analytics/models/staging/stg_smart_meter_readings.sql
+++
@@ -1,8 +0,0 @@
--- models/staging/stg_smart_meter_readings.sql
-SELECT
-  meter_id,
-  reading_time,
-  energy_kwh,
-  voltage,
-  current
-FROM "analytics"."public"."smart_meter_readings"
